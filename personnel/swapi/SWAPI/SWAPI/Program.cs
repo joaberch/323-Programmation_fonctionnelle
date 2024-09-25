b6fdf41a7a743b9294a67a8b25e3926b918f0efd
joaberch <122774888+joaberch@users.noreply.github.com>
2024-09-25 10:16:16 +0200

    chore(Newtonsoft.Json NuGet)

--- personnel/swapi/SWAPI/SWAPI/Program.cs
+++ personnel/swapi/SWAPI/SWAPI/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+
+const string baseAddress = "https://swapi.dev/";
+
+Console.WriteLine("Quel est le film Star Wars dont le titre est le plus long ?");
+
+string[] words = { "aba", "coucou" };
+Console.WriteLine(words.Where(word => !word.Contains('x')));
+
+Console.Read();
+
+using (var client = new HttpClient())
+{
+    using (var response = client.GetAsync(baseAddress).Result)
+    {
+        if (response.IsSuccessStatusCode)
+        {
+            var customerJsonString = await response.Content.ReadAsStringAsync();
+            //var cust = JsonConvert.DeserializeObject<Response>(customerJsonString);
+        }
+        else
+        {
+            Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+        }
+    }
+}
