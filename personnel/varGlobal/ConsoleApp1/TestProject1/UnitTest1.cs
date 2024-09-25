4aadd47f57c9634ea7bcc8b3cc479983e52d7f1a
joaberch <122774888+joaberch@users.noreply.github.com>
2024-09-25 11:50:22 +0200

    chore(add dependencies)

--- personnel/varGlobal/ConsoleApp1/TestProject1/UnitTest1.cs
+++ personnel/varGlobal/ConsoleApp1/TestProject1/UnitTest1.cs
@@ -4,8 +4,9 @@ namespace TestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestBob()
         {
+            Assert.AreEqual(1, 1);
         }
     }
 }
