771c9862eaa9e2309aa6b0ee1ebf9e1959a5e3f1
joaberch <122774888+joaberch@users.noreply.github.com>
2024-10-02 10:07:45 +0200

    feat(fileCount): count the numbers of file in the directory wanted

--- personnel/fileCount/ConsoleApp1/ConsoleApp1/Program.cs
+++ personnel/fileCount/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿int nbrFileCount = 0;
+const string PATH = "C:\\\\Temp";
+getFile(PATH);
+
+Console.Write($"nbr : {nbrFileCount}");
+Console.ReadLine();
+
+void getFile(string path)
+{
+    nbrFileCount += Directory.GetFiles(path).Count();
+    string[] directories = Directory.GetDirectories(path);
+    foreach (string directory in directories)
+    {
+        getFile(directory);
+    }
+}
