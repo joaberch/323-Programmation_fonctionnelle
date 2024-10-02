0cabe18c8a5ed6c1388c1ff83001a0e26714f156
joaberch <122774888+joaberch@users.noreply.github.com>
2024-10-02 10:28:06 +0200

    fix(fileCount purity): make the function pure

--- personnel/fileCount/ConsoleApp1/ConsoleApp1/Program.cs
+++ personnel/fileCount/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,16 +1,20 @@
-﻿int nbrFileCount = 0;
-const string PATH = "C:\\\\Temp";
-getFile(PATH);
+﻿const string PATH = "C:\\\\Temp";
+(int, int) file = getFile(PATH, (0, 0));
 
-Console.Write($"nbr : {nbrFileCount}");
+Console.WriteLine($"Dossier : {PATH}");
+Console.WriteLine($"{PATH} contient {file.Item1} fichiers et {file.Item2} dossiers");
 Console.ReadLine();
 
-void getFile(string path)
+(int, int) getFile(string path, (int , int ) nbr)
 {
-    nbrFileCount += Directory.GetFiles(path).Count();
+    nbr.Item1+= Directory.GetFiles(path).Count();
+    nbr.Item2+= Directory.GetDirectories(path).Count();
     string[] directories = Directory.GetDirectories(path);
-    foreach (string directory in directories)
+    foreach (var item in directories)
     {
-        getFile(directory);
+        (int, int) newFile = getFile(item, (0, 0));
+        nbr.Item1 += newFile.Item1;
+        nbr.Item2 += newFile.Item2;
     }
+    return (nbr.Item1, nbr.Item2);
 }
