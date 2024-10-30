6627a9b68eeb4895d1fae9d6af2c586724c62850
joaberch <122774888+joaberch@users.noreply.github.com>
2024-10-30 10:06:13 +0100

    chore(basis): Add the panel to draw following the point 2

--- personnel/fractales/KochSnowlake/KochSnowlake/Form1.Designer.cs
+++ personnel/fractales/KochSnowlake/KochSnowlake/Form1.Designer.cs
@@ -20,20 +20,35 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
         /// <summary>
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            //Panel
+            Panel drawingPanel = new Panel()
+            {
+                Location = new Point(90, 59),
+                Name = "drawingPanel",
+                Size = new Size(800, 600),
+                TabIndex = 0,
+            };
+            drawingPanel.Paint += DrawingPanel_Paint;
+            this.Controls.Add(drawingPanel);
+
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
         }
 
-        #endregion
+        private void DrawingPanel_Paint(object sender, PaintEventArgs e)
+        {
+            using (Pen pen = new Pen(Color.Blue, 1))
+            {
+                e.Graphics.DrawLine(pen, new Point(50, 50), new Point(75, 65));
+            }
+        }
     }
 }
